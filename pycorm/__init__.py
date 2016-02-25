1f0594ef2824da461acc4c62206d8b5d32685ac2
sloev <johannes.valbjorn@gmail.com>
2016-02-25 23:10:15 +0100

    Bump version: 0.2.2 → 0.2.3

--- pycorm/__init__.py
+++ pycorm/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Johannes Valbjørn'
 __email__ = 'johannes.valbjorn@gmail.com'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 from exceptions import PycormValidationError, PycormSchemaError
