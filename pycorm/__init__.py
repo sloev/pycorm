002df2bb5d8c7675f7a54f6561dff11b986d061e
sloev <johannes.valbjorn@gmail.com>
2016-02-28 14:25:24 +0100

    refactored a lot, introduced intelligent inheritance

--- pycorm/__init__.py
+++ pycorm/__init__.py
@@ -4,4 +4,3 @@ __author__ = 'Johannes Valbjørn'
 __email__ = 'johannes.valbjorn@gmail.com'
 __version__ = '0.2.7'
 
-from exceptions import PycormValidationError, PycormSchemaError
--- pycorm/pycorm.py
+++ pycorm/pycorm.py
@@ -8,8 +8,7 @@ def StringField():
 
 def NumberField():
     return {"type":"number"}
-def ModelField(model):
-    pass
+
 __allowed_options__ = ["additionalProperties", "required"]
 
 
@@ -19,13 +18,14 @@ class Model(dict):
 
     __getattr__= dict.__getitem__
 
-
-    def __init__(self, value_dict=None):
+    def __init__(self, value_dict=None, _recursive_constructor_obj=None):
         self.validate_schema()
-
         if value_dict is not None:
-            self.update(**dict((k, self.__parse_input(v))
-                           for k, v in value_dict.iteritems()))
+            if not _recursive_constructor_obj:
+                _recursive_constructor_obj = self.__schema__.get('properties', {})
+            self.update(**dict((k,
+                self.__parse_input(v, _recursive_constructor_obj.get(k, None)))
+                for k, v in value_dict.iteritems()))
         self.on_create()
 
     def on_create(self):
@@ -46,13 +46,13 @@ class Model(dict):
             kwargs = {key: cls.__parse_schema(cls.__popattr__(cls, key))
                     for key, value in cls.__dict__.items()
                     if not callable(value) and not str(key).startswith("__")}
-
             # create schema from kwargs and options
             cls.__schema__ = dict({
                 "type": "object",
                 "properties": kwargs
                 },
                 **options)
+            cls.__schema__ = cls(cls.__schema__)
         if not cls is Model and not cls.__schema_validator__:
             validator_class = jsonschema_validator_for(cls.__schema__)
             validator_class.check_schema(cls.__schema__)
@@ -68,20 +68,25 @@ class Model(dict):
     @classmethod
     def __parse_schema(cls, v):
         if isinstance(v, Model):
-            return v.__schema__
+            return v.__class__(v.__schema__)
         elif isinstance(v, list):
-            return [cls.parse(i) for i in v]
+            return [cls.__parse_schema(i) for i in v]
         else:
             return v
 
     @classmethod
-    def __parse_input(cls, v):
-        if isinstance(v, dict):
-            return cls(v)
+    def __parse_input(cls, v, _recursive_constructor_obj=None):
+        if isinstance(v, Model):
+            return v.__class__(v, _recursive_constructor_obj=_recursive_constructor_obj)
+        elif isinstance(v, dict):
+            if isinstance(_recursive_constructor_obj, Model):
+                return _recursive_constructor_obj.__class__(v, _recursive_constructor_obj=_recursive_constructor_obj)
+            return cls(v, _recursive_constructor_obj=_recursive_constructor_obj)
         elif isinstance(v, list):
-            return [cls.parse(i) for i in v]
+            return [cls.__parse_input(i) for i in v]
         else:
             return v
+
 
     @staticmethod
     def __popattr__(inst, key, default=None):
@@ -89,30 +94,3 @@ class Model(dict):
         if val is not None:
             delattr(inst, key)
         return val
-
-class A(Model):
-    required = ["foo"]
-    additionalProperties = False
-
-    foo = StringField()
-
-class B(Model):
-    baz = A()
-
-class C(Model):
-    __schema__ = {
-            "type": "object",
-            "properties": {
-                "foo": {"type": "string"}
-            },
-            "required": ["foo"],
-            "additionalProperties": False
-            }
-
-
-b = B.with_validation({"baz":A({"foo":"lolcat"})})
-#"raises exception - C.with_validation({"foo":10})
-c = C.with_validation({"foo":"lolcat"})
-
-print b.baz.foo == c.foo
-#pprint(b.baz.foo)
