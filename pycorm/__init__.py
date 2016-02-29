91e86b47e014652bd7394026b04a422cb23537a9
Johannes Valbjorn <jva@ordbogen.com>
2016-02-29 16:01:12 +0100

    Bump version: 0.2.10 → 0.2.11

--- pycorm/__init__.py
+++ pycorm/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = 'Johannes Valbjørn'
 __email__ = 'johannes.valbjorn@gmail.com'
-__version__ = '0.2.10'
+__version__ = '0.2.11'
 
 from pycorm import BaseModel, StringField, NumberField
