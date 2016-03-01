99eb8367ba913fbab4a8c2976af3a270240dea82
sloev <johannes.valbjorn@gmail.com>
2016-03-01 21:43:54 +0100

    Bump version: 0.2.12 → 0.2.13

--- pycorm/__init__.py
+++ pycorm/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = 'Johannes Valbjørn'
 __email__ = 'johannes.valbjorn@gmail.com'
-__version__ = '0.2.12'
+__version__ = '0.2.13'
 
 from exceptions import SchemaValidationError, InheritanceNotSupportedError
 from pycorm import BaseModel, StringField, NumberField
